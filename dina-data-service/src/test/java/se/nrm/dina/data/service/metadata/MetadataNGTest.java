68b41ea7196adf8e94f47e3fa105dd2b62f8768a
idali0226 <ida.li@nrm.se>
2016-08-31 13:55:42 +0200

    add keycloakAdmin.jar in travis file

--- dina-data-service/src/test/java/se/nrm/dina/data/service/metadata/MetadataNGTest.java
+++ dina-data-service/src/test/java/se/nrm/dina/data/service/metadata/MetadataNGTest.java
@@ -7,77 +7,86 @@ package se.nrm.dina.data.service.metadata;
 
 import java.util.List;
 import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import static org.testng.Assert.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import javax.servlet.http.HttpServletRequest; 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.testng.Assert.*; 
 import se.nrm.dina.data.service.vo.MetadataBean;
 
 /**
  *
  * @author idali
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MetadataNGTest {
     
+    @Mock
+    HttpServletRequest req;
+    
+    private String entity;
+    
+    private Metadata instance;
+    
     public MetadataNGTest() {
+        
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
+    @Before
+    public void setUp() throws Exception { 
+        instance = new Metadata();
+        entity = "Testentity";
     }
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
+    @After
+    public void tearDown() throws Exception { 
+        instance = null;
     }
 
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
-    }
 
     /**
      * Test of buildMetadata method, of class Metadata.
      */
-//    @Test
+    @Test
     public void testBuildMetadata_HttpServletRequest_String() {
+        
         System.out.println("buildMetadata");
-        HttpServletRequest req = null;
-        String entity = "";
-        Metadata instance = new Metadata();
-        MetadataBean expResult = null;
+    
+        when(req.getRequestURI()).thenReturn("/collections/v0/Testentity");
+        when(req.getContextPath()).thenReturn("/collections");
+         
         MetadataBean result = instance.buildMetadata(req, entity);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result); 
+        assertEquals("https://beta-api.dina-web.net/collections/v0/Testentity", result.getCallEndpoint()); 
+        assertEquals("0", result.getApiVersion());
     }
 
     /**
      * Test of buildMetadata method, of class Metadata.
      */
-//    @Test
+    @Test
     public void testBuildMetadata_9args() {
+        
         System.out.println("buildMetadata");
-        HttpServletRequest req = null;
-        String entity = "";
-        int offset = 0;
-        int limit = 0;
+        
+        when(req.getRequestURI()).thenReturn("/collections/v0/Testentity");
+ 
+        int offset = 20;
+        int limit = 20;
         String sortOrder = "";
         List<String> order = null;
         String orderby = "";
         boolean isExact = false;
         Map<String, String> condition = null;
-        Metadata instance = new Metadata();
-        MetadataBean expResult = null;
+        
+        instance = new Metadata();
+        
         MetadataBean result = instance.buildMetadata(req, entity, offset, limit, sortOrder, order, orderby, isExact, condition);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result); 
     }
     
 }
